99f482b0d5e37cafcecdc6931616ed54db9ef544
Sharmina Akhtar <sakht318@gmail.com>
2018-04-08 13:32:26 +0000

    tech controller

---
+++ app/controllers/techs_controller.rb
@@ -0,0 +1,90 @@
+class TechsController < ApplicationController
+
+  get '/techs' do
+    if logged_in?
+      @techs = Tech.all
+      erb :'techs/techs'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/techs/new' do
+    if logged_in?
+      erb :'/techs/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tech = Tech.find_by_id(params[:id])
+      erb :'techs/show_word'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/techs/:id/edit' do
+    if logged_in?
+      @tech = Tech.find_by_id(params[:id])
+      if @tech && @tech.user == current_user
+        erb :'techs/edit'
+      else
+        redirect '/techs'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  post'/techs' do
+    if logged_in?
+      if params[:tech][:title].empty? && params[:tech][:description].empty?
+        redirect "/techs/new"
+      else
+        @user = User.find_by(id: session[:user_id])
+        @tech = Tech.create(title: params[:title], description: params[:description], user_id: @user.id)
+        redirect to "/techs"
+      end
+     else
+      redirect '/login'
+     end
+  end
+
+  patch '/techs/:id' do
+    if logged_in?
+      if params[:tech][:title].empty? && params[:tech][:description].empty?
+        redirect "/techs/#{params[:id]}/edit"
+      else
+        @tech = Tech.find_by_id(params[:id])
+        if @tech && @tech.user = current_user
+          if @tech.update(:title => params[:title], :description => params[:description])
+            redirect to "/techs/#{@tech.id}"
+          else
+          redirect to "/techs/#{@tech.id}/edit"
+          end
+        else
+          redirect to '/techs'
+        end
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+
+  delete '/techs/:id/delete' do
+    if logged_in?
+      @tech = Tech.find_by_id(params[:id])
+      if @tech && @tech.user == current_user
+        @tech.delete
+      end
+      redirect '/techs'
+    else
+      redirect '/login'
+    end
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,33 @@
 class UsersController < ApplicationController
 
+  get '/users/:id' do
+    @user = User.find_by_id(params[:id])
+    erb :'users/tech_account'
+  end
+
   get "/signup" do
     if !logged_in?
-		   erb :"users/sign_up"
+		   erb :"users/signup"
     else
-      redirect '/lists'
+      redirect '/techs'
     end
 	end
 
   post "/signup" do
     if params[:username].empty? || params[:email].empty? || params[:password].empty?
-
         redirect "/signup"
     else
       @user = User.create(:username => params[:username], :email => params[:email],:password => params[:password])
       session[:user_id] = @user.id
-      redirect "/lists"
+      redirect "/techs"
     end
   end
 
   get "/login" do
     if !logged_in?
-		    erb :"users/sign_in"
+		    erb :"users/login"
     else
-      redirect '/lists'
+      redirect '/techs'
     end
 	end
 
@@ -32,7 +36,7 @@ class UsersController < ApplicationController
 		 if @user && @user.authenticate(params[:password])
 			 session[:user_id] = @user.id
 
-      redirect "/lists"
+      redirect "/techs"
      else
       redirect "/signup"
 		end
