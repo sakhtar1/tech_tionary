fc4baa235fa886fa9c6169a1a58ba02f92eedf97
Sharmina Akhtar <sakht318@gmail.com>
2018-04-08 11:56:59 +0000

    db

---
+++ db/migrate/20180408115521_create_techs.rb
@@ -0,0 +1,9 @@
+class CreateTechs < ActiveRecord::Migration[5.1]
+  def change
+      create_table :techs do |t|
+      t.string :title
+      t.string :description
+      t.integer :user_id
+    end
+  end
+end
