357bf26311382d6d5d3c9609c621b4a87e57aa25
Sharmina Akhtar <sakht318@gmail.com>
2018-04-11 13:44:32 +0000

    updating application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
-     set :public_folder, 'public'
      set :views, "app/views"
      enable :sessions
      set :session_secret, "secret"
